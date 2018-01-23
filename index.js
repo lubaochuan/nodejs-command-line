0f6b377d3582f71a9e0f64ba4323843b324847de
Baochuan Lu <blu@sbuniv.edu>
2018-01-23 11:51:39 +0000

    add color

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 'use strict'
 
 const program = require('commander')
+const chalk = require("chalk")
 const exec = require('child_process').exec
+const pkg = require('./package.json')
 
 let listFunction = (directory,options) => {
   const cmd = 'ls'
@@ -16,18 +18,25 @@ let listFunction = (directory,options) => {
   if (directory) fullCommand += ' ' + directory
 
   let execCallback = (error, stdout, stderr) => {
-    if (error) console.log("exec error: " + error)
-    if (stdout) console.log("Result: " + stdout)
-    if (stderr) console.log("shell error: " + stderr)
+    if (error) console.log(
+      chalk.red.bold.underline("exec error:") + error)
+    if (stdout) console.log(
+      chalk.green.bold.underline("Result:") + stdout)
+    if (stderr) console.log(
+      chalk.red("Error: ") + stderr)
   }
+
   exec(fullCommand, execCallback)
 }
 
 program
-  .version('0.0.1')
+  .version(pkg.version)
   .command('list [directory]')
   .description('List files and folders')
   .option('-a, --all','List all files and folders')
   .option('-l, --long','')
   .action(listFunction)
 program.parse(process.argv)
+
+// if program was called with no arguments, show help.
+if (program.args.length === 0) program.help()
